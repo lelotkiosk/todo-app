727a5e2651e69613dc9b25ec3ac8ddd6beba89f6
lelotkiosk <lelotletol@gmail.com>
2023-10-29 17:23:27 +0800

    2 - created model file, controller file, added api crud routes, and added crud methods on controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// get todos list
+Route::get('/todos', [TodoController::class, 'index']);
+// retrieve a single todo
+Route::get('/todos/{id}', [TodoController::class,'show']);
+// create a todo
+Route::post('/todos', [TodoController::class,'store']);
+// Update a todo
+Route::put('/todos', [TodoController::class,'update']);
+// delete a todo
+Route::delete('/todos/{id}', [TodoController::class,'destroy']);
